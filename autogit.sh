9fd9134783becca9e8e911416d390e58ea6b0739
Max Albrecht <1@eins78.com>
2012-08-05 20:07:50 +0300

    better message generation

--- autogit.sh
+++ autogit.sh
@@ -11,11 +11,21 @@ GIT_PATH="$1"
 # test
 echo "$GIT_PATH"
 
+# generate the message
+
+MESSAGE_TMP="autogit_$(date +"%s")"
+touch $MESSAGE_TMP
+### generate main commit message (followed by 2 linebreaks)
+echo -e "autocommit at $(date -u '+%FT%T+00:00') (UTC) by $USER on $HOSTNAME \n\n" >> $MESSAGE_TMP # fake RFC3339 date format as per http://stackoverflow.com/questions/11481115/bash-date-format-as-html5-datetime
+### generate more messages (comment out lines to disable)
+#### on OS X, ask iTunes for the current playing track
+osascript -e 'tell application "System Events" to if ((name of processes) contains "iTunes") then do shell script ("osascript -e " & quoted form of ("tell application \"iTunes\" to if player state is playing then \"iTunes is playing: \" & name of current track & \" - \" & artist of current track" & ""))'
+
 cd "$GIT_PATH"
 git add --all
 
 # only commit if neccessary (dirty check)
-git status | grep "nothing to commit (working directory clean)" || git commit -m "autocommit at $(date --rfc-3339=ns) by $USER on $HOSTNAME"
+git status | grep "nothing to commit (working directory clean)" || git commit -m "$MESSAGE_TMP"
 
 # push it to local or remote gitit wiki server
 git push
