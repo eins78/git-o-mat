4b5f84cfbb45f1546cfc35eeb0e2d68611159898
Max Albrecht <1@eins78.com>
2012-08-05 20:29:52 +0300

    fixing errors

--- autogit.sh
+++ autogit.sh
@@ -13,19 +13,19 @@ echo "$GIT_PATH"
 
 # generate the message
 
-MESSAGE_TMP="autogit_$(date +"%s")"
+MESSAGE_TMP="/tmp/autogit_$(date +"%s")"
 touch $MESSAGE_TMP
 ### generate main commit message (followed by 2 linebreaks)
 echo -e "autocommit at $(date -u '+%FT%T+00:00') (UTC) by $USER on $HOSTNAME \n\n" >> $MESSAGE_TMP # fake RFC3339 date format as per http://stackoverflow.com/questions/11481115/bash-date-format-as-html5-datetime
 ### generate more messages (comment out lines to disable)
 #### on OS X, ask iTunes for the current playing track
-osascript -e 'tell application "System Events" to if ((name of processes) contains "iTunes") then do shell script ("osascript -e " & quoted form of ("tell application \"iTunes\" to if player state is playing then \"iTunes is playing: \" & name of current track & \" - \" & artist of current track" & ""))'
+osascript -e 'tell application "System Events" to if ((name of processes) contains "iTunes") then do shell script ("osascript -e " & quoted form of ("tell application \"iTunes\" to if player state is playing then \"iTunes is playing: \" & name of current track & \" - \" & artist of current track" & ""))'  >> $MESSAGE_TMP
 
 cd "$GIT_PATH"
 git add --all
 
-# only commit if neccessary (dirty check)
-git status | grep "nothing to commit (working directory clean)" || git commit -F "$MESSAGE_TMP"
+# only commit if neccessary (dirty check) -- exit if not so no push
+git status | grep "nothing to commit (working directory clean)" && exit 0 || git commit -F "$MESSAGE_TMP"
 
 # push it to local or remote gitit wiki server
 git push
