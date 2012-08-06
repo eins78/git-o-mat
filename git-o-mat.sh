361bcbfff83a3cd28b50ba676688be14d90bf649
Max F. Albrecht <1@178.is>
2012-08-06 17:31:22 +0200

    change from 'bash' to 'sh' again
    in sh, echo need no -e
    
    Signed-off-by: Max F. Albrecht <1@178.is>

--- git-o-mat.sh
+++ git-o-mat.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # usage: autogit "/path/to/git/repository/"
 
@@ -13,7 +13,8 @@ git status | grep "nothing to commit (working directory clean)" >/dev/null && ex
 MESSAGE_TMP="/tmp/autogit_$(date +"%s")"
 touch $MESSAGE_TMP
 ### generate main commit message (followed by 2 linebreaks)
-echo -e "autocommit at $(date -u '+%FT%T+00:00') (UTC) by $USER on $HOSTNAME \n\n" >> $MESSAGE_TMP # fake RFC3339 date format as per http://stackoverflow.com/questions/11481115/bash-date-format-as-html5-datetime
+echo "autocommit at $(date -u '+%FT%T+00:00') (UTC) by $USER on $HOSTNAME \n\n" >> $MESSAGE_TMP # fake RFC3339 date format as per <stackoverflow.com/questions/11481115/bash-date-format-as-html5-datetime>
+
 ### generate more messages (comment out lines to disable)
 #### on OS X, ask iTunes for the current playing track
 osascript -e 'tell application "System Events" to if ((name of processes) contains "iTunes") then do shell script ("osascript -e " & quoted form of ("tell application \"iTunes\" to if player state is playing then \"iTunes is playing: \" & name of current track & \" - \" & artist of current track" & ""))'  >> $MESSAGE_TMP
