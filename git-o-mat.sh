dbb839ff9761627bf65b3c6bf92e981acf06f853
Max F. Albrecht <1@178.is>
2012-08-07 19:56:01 +0200

    - move message generating code out - more verbose output
    Signed-off-by: Max F. Albrecht <1@178.is>

--- git-o-mat.sh
+++ git-o-mat.sh
@@ -4,7 +4,10 @@
 
 GIT_PATH="$1"
 
-cd "$GIT_PATH" && git pull && git add --all || exit 1
+cd "$GIT_PATH"
+#&& git pull &&
+git add --all
+#|| exit 1
 # only work if neccessary (dirty check)
 git status | grep "nothing to commit (working directory clean)" && exit 0
 
@@ -15,10 +18,13 @@ touch $MESSAGE_TMP
 ### generate main commit message (followed by 2 linebreaks)
 echo "autocommit at $(date -u '+%FT%T+00:00') (UTC) by $USER on $HOSTNAME \n\n" >> $MESSAGE_TMP # fake RFC3339 date format as per <stackoverflow.com/questions/11481115/bash-date-format-as-html5-datetime>
 
-### generate more messages (comment out lines to disable)
-#### on OS X, ask iTunes for the current playing track
-osascript -e 'tell application "System Events" to if ((name of processes) contains "iTunes") then do shell script ("osascript -e " & quoted form of ("tell application \"iTunes\" to if player state is playing then \"iTunes is playing: \" & name of current track & \" - \" & artist of current track" & ""))'  >> $MESSAGE_TMP
+### generate more messages if there is a script for it in the repo
 
+[[ -f ".papermill/message.sh" ]] && sh .papermill/message.sh >> $MESSAGE_TMP
+
+echo "[#] Commiting to Git ..."
+echo "    [:] Message:"
+echo "$MESSAGE_TMP"
 git commit -F "$MESSAGE_TMP" ## commit with $MESSAGE
 
 git push ## push it to default git repo origin (local or remote)
